54a13b21f8dd3ec7a534fd02b21954114e7f4a74
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2023-12-27 11:02:54 +0100

    fix(abel): comments

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,19 @@
 """
     This is the main module to execute the REST API using AMPQ
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 from src.rest_api_agr.routes import *
 from src.rest_api_agr.AMQP import declare_thread_ampq
